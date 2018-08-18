cfa290eb4c8e39ede9582226f6f24121c5000168
Mohammad Javadi <javadi91.5@gmail.com>
2018-08-18 11:55:15 +0430

    Initializing

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @author Mohammad Javadi
+ * @version 1.0.0
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
